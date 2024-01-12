ebee20e526410f6061edf2af60bb5230d688832e
Victor Moreno <>
2024-01-12 12:27:19 +0100

    test: generates random word

--- hangman/dictionary/test/dictionary_test.exs
+++ hangman/dictionary/test/dictionary_test.exs
@@ -2,7 +2,7 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "greets the world" do
-    assert Dictionary.hello() == :world
+  test "generates a random word" do
+    assert Dictionary.random_word() !== nil
   end
 end
