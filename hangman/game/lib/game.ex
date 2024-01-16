06ee626d86cf5cbb31af265180ad99586e20ca69
Victor Moreno <victor.moreno@cabify.com>
2024-01-16 12:04:48 +0100

    fix: rename Game to game as type

--- hangman/game/lib/game.ex
+++ hangman/game/lib/game.ex
@@ -1,7 +1,18 @@
 defmodule Game do
+  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type game :: any
+  @type tally :: %{
+          turns_left: integer,
+          game_state: state,
+          guesses: list(String.t()),
+          used: list(String.t())
+        }
+
+  @spec new() :: game
   def new do
   end
 
+  @spec make_guess(game, String.t()) :: {game, tally}
   def make_guess(_game, _guess) do
   end
 end
