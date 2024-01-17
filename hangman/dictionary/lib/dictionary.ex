1049eb3b81c1965df324b482a7345cb342a0be05
Victor Moreno <victor.moreno@cabify.com>
2024-01-17 13:43:21 +0100

    refactor: create types for dictionary

--- hangman/dictionary/lib/dictionary.ex
+++ hangman/dictionary/lib/dictionary.ex
@@ -1,7 +1,9 @@
 defmodule Dictionary do
   alias Dictionary.Impl.WordList
-  @spec start() :: [String.t()]
-  defdelegate start, to: WordList
-  @spec random_word(word_list :: [String.t()]) :: String.t()
+  @opaque t :: WordList.t()
+
+  @spec start() :: t
+  defdelegate start, to: WordList, as: :word_list
+  @spec random_word(word_list :: t) :: String.t()
   defdelegate random_word(word_list), to: WordList
 end
--- hangman/dictionary/lib/impl/word_list.ex
+++ hangman/dictionary/lib/impl/word_list.ex
@@ -1,10 +1,14 @@
 defmodule Dictionary.Impl.WordList do
-  def start() do
+  @type t :: [String.t()]
+
+  @spec word_list() :: t
+  def word_list() do
     "assets/words.txt"
     |> File.read!()
     |> String.split("\n", trim: true)
   end
 
+  @spec random_word(t) :: String.t()
   def random_word(word_list) do
     word_list |> Enum.random()
   end
